96d1e4676fef1942e25e33a69ce83b2f17d37832
Martin Othamar <Martin.Othamar@dnv.com>
2022-05-28 12:44:49 +0200

    useless with( pattern?

---
+++ rust/sdk/benches/README.md
@@ -0,0 +1,10 @@
+## Running benchmarks
+
+```bash
+cargo bench --bench benchmarks
+
+cargo bench --bench benchmarks -- --profile-time 10
+
+# or cargo criterion - install: cargo install cargo-criterion
+cargo criterion
+```
---
+++ rust/sdk/benches/benchmarks.rs
@@ -0,0 +1,27 @@
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use vista_sdk::{gmod::{TraversalHandlerResult}, vis::Vis, vis::VisVersion};
+
+use pprof::criterion::{Output, PProfProfiler};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let instance = Vis::instance();
+    let gmod = instance.get_gmod(VisVersion::v3_4a);
+
+    c.bench_function("gmod full traversal", |b| b.iter(|| {
+        gmod.traverse(|_parents, _node| {
+            TraversalHandlerResult::Continue
+        });
+    }));
+}
+
+criterion_group!{
+    name = benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(30))
+        .sample_size(500)
+        .with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = criterion_benchmark
+}
+criterion_main!(benches);
