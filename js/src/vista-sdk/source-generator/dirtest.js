e2c61c952419d901241ffbde13343beb9b49c242
Menezes <Ezer.Menezes@dnv.com>
2022-06-03 14:56:13 +0200

    Updating pipeline

--- js/src/vista-sdk/source-generator/dirtest.js
+++ js/src/vista-sdk/source-generator/dirtest.js
@@ -1,2 +1,14 @@
-const checkDir = () => console.log(`__dirname: [${__dirname}] end__dirname`);
+const checkDir = () => {
+    const fs = require("fs-extra");
+    try {
+        console.log(fs.readdirSync("./"));
+    } catch (ex) {}
+
+    try {
+        console.log(fs.readdirSync("./resources"));
+    } catch (ex) {}
+
+    console.log(`__dirname: [${__dirname}] end__dirname`);
+};
+
 checkDir();
