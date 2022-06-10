7753ce9f8805000faa9b958b77b7fe557491f2d1
Duy Tan Tran <duy.tan.huynh.tran@dnv.com>
2022-06-10 16:27:18 +0200

    refactor codebook tests

--- csharp/test/Vista.SDK.Tests/VistaSDKTestData.cs
+++ csharp/test/Vista.SDK.Tests/VistaSDKTestData.cs
@@ -5,58 +5,33 @@ namespace Vista.SDK.Tests;
 
 public class VistaSDKTestData
 {
-    public static IEnumerable<object[]> AddValidPositionData()
-    {
-        foreach (var vp in CodebookTestData.ValidPosition)
-        {
-            yield return vp;
-        }
-    }
+    public static IEnumerable<object[]> AddValidPositionData() =>
+        AddData(CodebookTestData.ValidPosition);
 
-    public static IEnumerable<object[]> AddStatesData()
-    {
-        foreach (var state in CodebookTestData.States)
-        {
-            yield return state;
-        }
-    }
+    public static IEnumerable<object[]> AddStatesData() => AddData(CodebookTestData.States);
 
-    public static IEnumerable<object[]> AddPositionsData()
-    {
-        foreach (var position in CodebookTestData.Positions)
-        {
-            yield return position;
-        }
-    }
+    public static IEnumerable<object[]> AddPositionsData() => AddData(CodebookTestData.Positions);
 
-    public static IEnumerable<object[]> AddTagData()
-    {
-        foreach (var tag in CodebookTestData.Tag)
-        {
-            yield return tag;
-        }
-    }
+    public static IEnumerable<object[]> AddTagData() => AddData(CodebookTestData.Tag);
 
-    public static IEnumerable<object[]> AddDetailTagData()
-    {
-        foreach (var tag in CodebookTestData.DetailTag)
-        {
-            yield return tag;
-        }
-    }
+    public static IEnumerable<object[]> AddDetailTagData() => AddData(CodebookTestData.DetailTag);
 
     private static VistaTestData CodebookTestData => GetData("Codebook");
 
     private static VistaTestData GetData(string testName)
     {
-        var resourcePath = $@"..\..\..\testdata\{testName}.json";
-        var path = Path.GetFullPath(
-            Path.Combine(resourcePath, $@"..\..\..\..\..\testdata\{testName}.json")
-        );
-
+        var path = $"testdata/{testName}.json";
         var testDataJson = File.ReadAllText(path);
 
         return JsonSerializer.Deserialize<VistaTestData>(testDataJson)!;
+    }
+
+    public static IEnumerable<object[]> AddData(string[][] data)
+    {
+        foreach (var state in data)
+        {
+            yield return state;
+        }
     }
 }
 
