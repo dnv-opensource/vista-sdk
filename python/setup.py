abfc2e0a4036d03786b4d286ee1d41f4d011ba36
Fredriksen <Anders.Fredriksen@dnv.com>
2024-07-04 13:14:24 +0200

    fixes to build

--- python/setup.py
+++ python/setup.py
@@ -13,19 +13,6 @@ if len(sys.argv) > 1:
             version += arg.split("=")[1]
             sys.argv.remove(arg)
 
-resource_directory = os.path.join(os.path.dirname(__file__), '..', 'resources')
-
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            # Add relative path to the files
-            paths.append(os.path.relpath(os.path.join(path, filename), os.path.dirname(__file__)))
-    return paths
-
-
-resource_files = package_files(resource_directory)
-
 setup(
     name="vista-sdk",
     version=version,
@@ -36,7 +23,9 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    package_data={'resources': resource_files},
+    package_data={
+        'vista_sdk': ['resources/*'],
+    },
     install_requires=['cachetools>=5.3.3','parameterized>=0.9.0','pydantic>=2.7.1'],
     classifiers=[
         "Programming Language :: Python :: 3",
