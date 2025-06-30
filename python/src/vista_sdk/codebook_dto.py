2693aa1e7c63ee5ebe4c1d512ff4ab1cddc6b0e5
Michael Ekornrud <ole.michael.ekornrud@dnv.com>
2025-06-30 09:48:57 +0200

    Fullført implementassjon av codebooks

--- python/src/vista_sdk/codebook_dto.py
+++ python/src/vista_sdk/codebook_dto.py
@@ -1,17 +1,17 @@
 """Data trandfer object for the condebooks and codebook."""
 
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 
-class CodeBookDto:
+class CodebookDto(BaseModel):
     """Codebook DTO."""
 
     name: str = Field(..., alias="name")
     values: dict[str, list[str]] = Field(..., alias="values")
 
 
-class CodeBooksDto:
+class CodebooksDto(BaseModel):
     """Codebooks DTO."""
 
     vis_release: str = Field(..., alias="visRelease")
-    items: list[CodeBookDto] = Field(..., alias="items")
+    items: list[CodebookDto] = Field(..., alias="items")
