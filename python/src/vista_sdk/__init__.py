e1cace19ca402630084a56432d62a3d225b50bb8
Kristoffer <kristoffer.saastad@dnv.com>
2026-01-27 10:58:45 +0100

    feat: workflow base version

--- python/src/vista_sdk/__init__.py
+++ python/src/vista_sdk/__init__.py
@@ -3,3 +3,6 @@
 This package provides Python bindings for the Vista SDK, enabling
 standardized vessel data integration using the DNV Vessel Information Structure (VIS).
 """
+
+# This will be replaced during packaging/building
+__version__ = "0.2.0-preview.0"
